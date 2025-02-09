8524f75abe759c2d5cab5c7a622d80e2c51eadd9
30029533 <patel.jay3009@gmail.com>
2025-02-09 17:11:13 +1300

    - Enhance Card component with like functionality and click counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import PropTypes from 'prop-types'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
-const hasLiked = true;
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${ hasLiked }`);
+  }, [hasLiked]);
+
+  useEffect(() => {
+    console.log('Card Rendered');
+  });
 
   return (
-    <div className='card' >
-      <h2>{ title }</h2>
+    <div className='card' onClick={ () => setCount(count + 1) }>
+      <h2>{ title } <br /> { count || null }</h2>
       <button onClick={ () => setHasLiked(!hasLiked) }>
         { hasLiked ? '💖' : '🤍' }
       </button>
