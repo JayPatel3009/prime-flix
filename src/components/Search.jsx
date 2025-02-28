d0004746c968b9e269784c828b202f55ae2ac558
30029533 <patel.jay3009@gmail.com>
2025-02-28 21:06:43 +1300

    Refactor state management and API handling
    - Refactored `App.jsx` to use `MoviesProvider` for search term state and custom hooks (`useMovies`, `useTrendingMovies`) for fetching movies.
    - Centralized API-related constants and functions in `api.js`.
    - Improved readability and maintainability of `appwrite.js`.
    - Reformatted JSX in `MovieCard.jsx`, `Search.jsx`, and `Spinner.jsx`.
    - Added `MoviesContext.jsx` for context management and new custom hooks for fetching movies.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { BASE_URL } from '../utils'
+import React from 'react';
+import { BASE_URL } from '../utils';
 
 const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
-        <div>
-            <img src={`${BASE_URL}/search.svg`} alt="search" />
-
-            <input
-                type="text"
-                placeholder="Search through millions of movies..."
-                value={ searchTerm }
-                onChange={ (event) => setSearchTerm(event.target.value) }
-            />
-        </div>
+      <div>
+        <img src={`${BASE_URL}/search.svg`} alt="search" />
+        <input
+          type="text"
+          placeholder="Search through millions of movies..."
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
