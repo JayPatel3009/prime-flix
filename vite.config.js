70422acc44d1e923931a925a36dd755c3c9f6373
30029533 <patel.jay3009@gmail.com>
2025-03-01 19:20:50 +1300

    Update imports, image paths, and config for deployment
    - Simplified imports in App.jsx by directly importing useContext and useState from 'react'.
    - Updated image source paths to relative paths in App.jsx, MovieCard.jsx, and Search.jsx for better portability.
    - Modified base URL in vite.config.js for deployment.
    - Added new image asset text-logo_2.png.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/prime-flix',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
